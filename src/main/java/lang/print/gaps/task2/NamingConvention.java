8bb420391aec7cb9e2273c725979111ec9560329
wilkat <katarzyna.f.wilk@gmail.com>
2022-10-30 21:03:24 +0100

    new

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,14 +2,14 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int variable = 10;
-        int variable_one = 1;
-        int variableTwo = 2;
-        int variable_three = 3;
+        int variable;
+        int variable_one;
+        int variableTwo;
+        int variable_three;
         final int VARIABLE_FOUR = 4;
         final int VARIABLE_FIVE = 5;
-        int variableSix = 6;
-        int variableSeven = 7;
-        int variableEight = 8;
+        int variableSix;
+        int variableSeven;
+        int variableEight;
     }
 }
